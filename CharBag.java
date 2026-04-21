dc5401eb3c8d929f7ad270659424c8b8cbd2106a
aendres527 <endre095@umn.edu>
2026-04-21 16:54:30 -0500

    done with charBag
    finished char bag, now starting TrieNode

--- CharBag.java
+++ CharBag.java
@@ -1,48 +1,110 @@
 public class CharBag {
     
-    private char[] bag;
-    private int[] count;
-    private int index;
+    private int[] bag;
+    private int netChanges;
 
 
     public CharBag() {
-        bag = new char[1000];
-        count = new int[27];
-        index = 0;
+        bag = new int[27];
+        netChanges = 0;
+
     }
+    /*
+     * creates the charBag object and setting default vals
+     */
 
-    public int getCount(char c){ return 0;};
-
-    public void add(char c){
-        if (Character.isUpperCase(c)) {
+    public int getCharIndex(char c) {
+        if (Character.isUpperCase(c)) { //converst the char to lowercase if not already
             c = Character.toLowerCase(c);
         }
-        int asciVal = (int) c;
-        
-
-        if (asciVal >= 141 && asciVal <= 172) {
-            bag[index] = c;
-            index++;
-            return;
+        int index = c - 'a'; //normalizes the characters asci value into its array index
+        if (index >= 26 || index < 0) {
+            return 26; //returns the '.' character
         }
-        else {
-            bag[index] = '.';
-            index++;
-        }
+        return index; //else returns the characters index thats within the array's bounds
     }
+    /*
+     * converts the char to lowercase if it already isnt, then it normalizes the
+     * char's ascii val to find its array index, if it is out of range of the
+     * alphabet it returns the index for ., else it returns the chars corresponding
+     * index
+     */
+    
+
+    public void add(char c){
+        int index = getCharIndex(c);
+        bag[index]++;
+        netChanges++;
+    }
+    /*
+     * adds a character by finding the index associated with the character and
+     * incremengint it
+     */
 
     public void remove(char c){
-        for (char curr: bag) {
-            if (curr == c) {
-                bag.remove()
-            }
+        int index = getCharIndex(c);
+        if (bag[index] > 0) {
+            bag[index]--;
+            netChanges--;
         }
     }
+    /*
+     * removes a characters from the bag by using its index and decrementing the
+     * count associated with the index
+     */
 
 
-    public int getSize(){ return 0;}
-    public String toString() {return "";}
-    public char getRandomChar() {return 'x';}
+    public int getCount(char c) {
+        int index = getCharIndex(c);
+        return bag[index];
+    }
+    
+    public int getSize() {
+        return netChanges;
+    }
 
+    public  String toString() {
+        String returnStr = "CharBag{";
+        for (int i = 0; i < bag.length; i++) {
+            char currChar = (char) (i + 97);
+            if (i == 26) {
+                returnStr += (LetterSample.STOP + ":" + bag[i]);
+            }
+            else {
+                returnStr += (currChar + ":" + bag[i] + ", ");
+            }
+        }
+        return returnStr + "}";
+    }
+    /*
+     * prints the characters and their frequency
+     */
 
+    public char getRandomChar() {
+        if (getSize() <= 0) {
+            return LetterSample.STOP; // or '.'
+        }
+
+        int count = (int) (Math.random() * getSize()); // random number [0, size)
+
+        for (int i = 0; i < bag.length; i++) {
+            count -= bag[i];
+            if (count < 0) {
+                if (i == 26)
+                    return LetterSample.STOP;
+                return (char) (i + 'a');
+            }
+        }
+
+        return LetterSample.STOP; // fallback (should never happen)
+    }
+    /*
+ * if there were never any changes made to the array, it returns the stop char,
+ * then it finds a random number from 0 to the netChanges-1, and loops through
+ * the bag subtracting the values in each arary slot from count, if count is < 0
+ * it finds a value to return, this makes it so that there is a weight based on
+ * how many times a char shows up, if it shows up more it subtracts more from
+ * the count and therefor is more likely to make the count < 0, which will pick
+ * the char
+ */
 }
